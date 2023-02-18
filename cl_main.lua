7ebe2bcac8f1e61096465a7ac1b1fbe410873da5
John Doe <shellbd22@gmail.com>
2023-02-18 13:48:03 +0700

    update hud

---
+++ cl_main.lua
@@ -0,0 +1,96 @@
+ESX = exports['es_extended']:getSharedObject()
+
+local PlayerData = {}
+local maxPlayers = GetConvar("sv_maxclients", "128")
+local PlayerData = {}
+local player_fps = 0
+local fps_calc = 0
+local newping = 0
+local jancok
+local newexp = 0
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+	ESX.PlayerData = xPlayer
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+	ESX.PlayerData.job = job
+
+    jancok = ESX.PlayerData.job.label .. " - " .. ESX.PlayerData.job.grade_label
+end)
+
+RegisterNetEvent("HandlePing")
+AddEventHandler("HandlePing", function(ping)
+    newping = ping
+end)
+
+-- RegisterCommand("exp", function()
+--     ESX.TriggerServerCallback('esx_crafting:GetSkillLevel', function(level)
+--         newexp = level
+--     end)
+
+--     ExecuteCommand('me Exp : ' .. newexp)
+-- end)
+
+RegisterNUICallback('go_hud:HandInfo', function(data, cb)
+    cb({
+        -- player = GetPlayerName(PlayerId()),
+        player = xPlayer.getName(),
+        player_job = ESX.PlayerData.job.label .. " - " .. ESX.PlayerData.job.grade_label,
+    --  playerpingofc = newexp,
+        ID = GetPlayerServerId(PlayerId()),
+
+        -- ICONS --
+        ID_ICON = Config.Icons.ID_ICON,
+        PLAYERS_ICON = Config.Icons.PLAYERS_ICON,
+        LOBBY_ICON = Config.Icons.LOBBY_ICON,
+        FPS_ICON = Config.Icons.FPS_ICON,
+        DISCORD_ICON = Config.Icons.DISCORD_ICON,
+        WEBSITE_ICON = Config.Icons.WEBSITE_ICON
+    })
+end)
+
+--[[ CreateThread(function()
+    while true do
+        fps_calc = fps_calc + 1
+        Wait(0)
+    end
+end) --]]
+
+CreateThread(function() -- calc
+    while true do
+        player_fps = fps_calc
+        fps_calc = 0
+
+        Wait(1000)
+    end
+end)
+
+RegisterNUICallback('ready', function(data, cb)
+    cb("ok")
+end)
+
+RegisterCommand("testp", function(source)
+    TriggerServerEvent("GetPlayerPing")
+    return newping
+end)
+
+TriggerEvent('chat:addSuggestion', '/moshhud', 'hide or show', {
+    { name="hide or show", help="hides / shows the top hud" },
+})
+
+RegisterCommand("moshhud", function(source, args)
+    if args[1] == "hide" then
+        TriggerEvent("hidevehhud", source)
+    SendNUIMessage({
+        action = "hide_hud"
+    })
+    elseif  args[1] == "show" then
+        TriggerEvent("showevehhud", source)
+        SendNUIMessage({
+            action = "show_hud"
+        })
+    end
+end)
---
+++ nui/js/main.js
@@ -0,0 +1,87 @@
+const maxLength = 18 // To prevent long names from runing the hud
+
+var app = new Vue({
+    el: '#initapp',
+    data: {
+        serverName: "#DIMANA",
+        server_logo: "https://cdn.discordapp.com/attachments/997544666801119302/1072160093744930878/gta_PNG15.png",
+        player: 'G0ogle',
+        player_fps: 90,
+        playerpingofc: 11,
+        ID: 2,
+        discord: "https://discord.gg/SqrYEXaMnQ",
+        website_link: "https://www.genesisys.com/",
+        active_players: 0,
+        player_job : "Unemployed",
+
+
+        // ICONS 
+        ID_ICON: "",
+        PLAYERS_ICON: "",
+        LOBBY_ICON: "",
+        FPS_ICON: "",
+        DISCORD_ICON: "",
+        WEBSITE_ICON: "",
+
+
+        elements: []
+    },
+    computed: {
+        serverPlayers: function() {
+            if(this.player.length > maxLength) {
+                return this.player.substring(0, maxLength - 3) + ".."
+            } else {
+                return this.player
+            }
+        }
+    }
+})
+
+const getEvent = (event, data = {}, cb = (() => {})) => {
+    fetch(`https://${GetParentResourceName()}/${event}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify(data)
+    }).then(response => response.json()).then(response => {
+        cb(response)
+    })
+}
+
+function keepitUpdated() {
+    getEvent("go_hud:HandInfo", {}, response => {
+        Object.keys(response).forEach(key => {
+            let value = response[key]
+
+            if(key == "player_job") {
+                if(value < 10) {
+                    value = `${value}&nbsp;&nbsp;`
+                } else if(value < 100) {
+                    value = `${value}&nbsp;`
+                }
+            }
+            app[key] = value
+        })
+    })
+}
+
+keepitUpdated()
+setInterval(keepitUpdated, 1000)
+
+
+window.addEventListener('message', function(event) {
+    const data = event.data;
+
+    if(data.action) {
+        if(data.action == "hide_hud") {
+            var x = document.getElementById("initapp");
+            x.style.display = "none";
+        } else if (data.action == "show_hud") {
+            var x = document.getElementById("initapp");
+            x.style.display = "block";
+        }
+    }
+});
+
+getEvent("ready")
